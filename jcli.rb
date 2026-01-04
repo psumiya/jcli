97141364e4b5cb470caddaa1f8e86ceca23a1f12
psumiya <sumiya.p@gmail.com>
2026-01-03 18:24:22 -0800

    fix: Update formula URLs to v0.0.1-beta

--- jcli.rb
+++ jcli.rb
@@ -1,13 +1,13 @@
 class Jcli < Formula
   desc "Sugared wrappers to execute methods from JDK and JVM-based libraries from the command line"
   homepage "https://github.com/psumiya/jcli"
-  version "0.1"
+  version "0.0.1-beta"
 
   if Hardware::CPU.intel?
-    url "https://github.com/psumiya/jcli/releases/download/v0.1/jcli-0.1-osx-x86_64.tar.gz"
+    url "https://github.com/psumiya/jcli/releases/download/v0.0.1-beta/jcli-0.0.1-beta-osx-x86_64.tar.gz"
     sha256 "132de7b62340934215b84fa935b9258edd4d01fe32b105d59f04811fa9d655ee"
   else
-    url "https://github.com/psumiya/jcli/releases/download/v0.1/jcli-0.1-osx-aarch64.tar.gz"
+    url "https://github.com/psumiya/jcli/releases/download/v0.0.1-beta/jcli-0.0.1-beta-osx-aarch64.tar.gz"
     sha256 "REPLACE_WITH_SHA256_OF_ARM64_TARBALL"
   end
 
